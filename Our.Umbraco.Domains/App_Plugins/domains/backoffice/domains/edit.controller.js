ca4b356d26d9532a88b4ac192ee9dad98d7e2a42
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2020-08-13 12:39:27 +0200

    Languages
    Get installed languages

--- Our.Umbraco.Domains/App_Plugins/domains/backoffice/domains/edit.controller.js
+++ Our.Umbraco.Domains/App_Plugins/domains/backoffice/domains/edit.controller.js
@@ -1,20 +1,51 @@
 ﻿(function () {
     'use strict';
 
-    function controller($location) {
+    function controller($location, languageResource, localizationService) {
 
         var vm = this;
+        vm.labels = [];
         vm.page = {};
         vm.showBackButton = true;
-        
+        vm.availableLanguages = null;
+
+        vm.domain = {};
 
         vm.back = back;
 
-        
+
+        init();
+
+        function init() {
+            vm.loading = true;
+
+            var labelKeys = [
+                "default_addDomain"
+                ];
+           
+            localizationService.localizeMany(labelKeys).then(function (values) {
+                vm.labels.addDomain = values[0];
+                // set page name
+                vm.page.name = vm.labels.addDomain;
+            });
+            
+
+            languageResource.getAll().then(function (languages) {
+                vm.availableLanguages = languages;
+
+                vm.loading = false;
+            });
+
+            vm.loading = false;
+
+        }
+
+
+
         function back() {
             $location.path("settings/domains/dashboard");
         }
-        
+
     }
 
     angular.module('umbraco')
